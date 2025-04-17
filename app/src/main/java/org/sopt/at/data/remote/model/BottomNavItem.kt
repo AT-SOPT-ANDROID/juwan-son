4268a365c2d275eb0c31c8a48009497378df0f44
Son Juwan <wndhks171@naver.com>
2025-04-18 04:16:45 +0900

    "[Feat/#week2] components item (dto)"

---
+++ app/src/main/java/org/sopt/at/data/remote/model/BottomNavItem.kt
@@ -0,0 +1,7 @@
+package org.sopt.at.data.remote.model
+
+data class BottomNavItem(
+    val name: String,
+    val iconRes: Int,
+    val selectedIconRes: Int? = null
+)
---
+++ app/src/main/java/org/sopt/at/data/remote/model/LiveItem.kt
@@ -0,0 +1,7 @@
+package org.sopt.at.data.remote.model
+
+data class LiveItem(
+    val rank: Int,
+    val title: String,
+    val imageRes: Int
+)

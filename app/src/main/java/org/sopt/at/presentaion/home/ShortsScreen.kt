f1c9d7e860a33f4b3c9f19f5aeedcf1783b9c1e4
Son Juwan <wndhks171@naver.com>
2025-04-18 04:17:34 +0900

    "[Feat/#week2] Shortsscreen"

---
+++ app/src/main/java/org/sopt/at/presentaion/home/ShortsScreen.kt
@@ -0,0 +1,42 @@
+package org.sopt.at.presentaion.home
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import org.sopt.at.presentaion.home.ui.theme.ATSOPTANDROIDTheme
+import org.sopt.at.presentation.home.TopTabBar
+
+@Composable
+fun ShortsScreen() {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .background(color= Color.Black)
+            .fillMaxSize()
+    ) {
+        Text(
+            color = Color.White,
+            text="SHORTS",
+
+            )
+
+    }
+}
+
+

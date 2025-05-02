6da0b4808087c1d9c44f8d79ab5fdb5bc400aa6a
Son Juwan <wndhks171@naver.com>
2025-05-02 21:44:52 +0900

    "[Feat/#week3] Theme (color+Typography"

--- app/src/main/java/org/sopt/at/ui/theme/Theme.kt
+++ app/src/main/java/org/sopt/at/ui/theme/Theme.kt
@@ -9,7 +9,60 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.platform.LocalView
+import androidx.compose.runtime.SideEffect
+import androidx.core.view.WindowCompat
+
+object TivingTheme {
+    val colors: TivingColors
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalTivingColorsProvider.current
+
+    val typography: TivingTypography
+        @Composable
+        @ReadOnlyComposable
+        get() = LocalTivingTypographyProvider.current
+}
+
+@Composable
+fun ProvideTivingColorsAndTypography(
+    colors: TivingColors,
+    typography: TivingTypography,
+    content: @Composable () -> Unit
+) {
+    CompositionLocalProvider(
+        LocalTivingColorsProvider provides colors,
+        LocalTivingTypographyProvider provides typography,
+        content = content
+    )
+}
+
+@Composable
+fun TivingTheme(
+    content: @Composable () -> Unit
+) {
+    ProvideTivingColorsAndTypography (
+        colors = defaultTivingColors,
+        typography = defaultTivingTypography
+    ) {
+        val view = LocalView.current
+        if (!view.isInEditMode) {
+            SideEffect {
+                (view.context as Activity).window.run {
+                    WindowCompat.getInsetsController(this, view).isAppearanceLightStatusBars = false
+                }
+            }
+        }
+        MaterialTheme(
+            content = content
+        )
+    }
+}
+
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,

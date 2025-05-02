d84069eb21fd66c2a37216e0debb34f635dade77
Son Juwan <wndhks171@naver.com>
2025-05-02 21:43:35 +0900

    "[Feat/#week3] dependency"

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -52,6 +52,7 @@ dependencies {
     implementation(libs.androidx.material3)
     implementation(libs.transport.runtime)
     implementation(libs.androidx.runtime.livedata)
+    implementation(libs.androidx.media3.common.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
